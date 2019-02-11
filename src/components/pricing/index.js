99c2a0ccb6233351e6e575fe48958178a637aeb2
maracujanna <jannabrilyantova@gmail.com>
2019-02-11 01:29:05 -0200

    Add Pricing component

---
+++ src/components/pricing/index.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import Zoom from 'react-reveal/Zoom'
+
+import MyButton from '../utils/MyButton'
+
+class Pricing extends Component {
+    state = {
+        prices:[100,150,250],
+        positions:['Balcony','Medium', 'Star'],
+        desc: [
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'Officiis modi pariatur fugiat neque a provident sed, maiores aperiam blanditiis.',
+            'Earum perferendis architecto atque cumque repellat asperiores placeat sequi reiciendis dolor!'
+        ],
+        linkTo:['http://sales/b','http://sales/m','http://sales/s'],
+        delay: [500, 0, 500]
+    }
+
+    showBoxes = () => (
+        this.state.prices.map((box, i) => (
+            <Zoom
+                delay={this.state.delay[i]}
+                key={i}
+            >
+                <div className="pricing_item">
+                    <div className="pricing_inner_wrapper">
+                        <div className="pricing_title">
+                            <span>${this.state.prices[i]}</span>
+                            <span>{this.state.positions[i]}</span>
+                        </div>
+                        <div className="pricing_description">
+                            <span>{this.state.desc[i]}</span>
+                        </div>
+                        <div className="pricing_buttons">
+                            <MyButton 
+                                text="Purchase"
+                                bck="#ffa800"
+                                color="#fff"
+                                link={this.state.linkTo[i]}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </Zoom>
+        ))
+    )
+
+    render() {
+        return (
+        <div className="bck_black">
+            <div className="center_wrapper pricing_section">
+                <h2>Pricing</h2>
+
+                <div className="pricing_wrapper">
+                    {this.showBoxes()}
+                </div>
+
+            </div>
+        </div>
+        )
+    }
+}
+export default Pricing
