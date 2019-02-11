d05b29c8dbc4f67eada98e544fda76142f245e89
maracujanna <jannabrilyantova@gmail.com>
2019-02-11 00:56:54 -0200

    Add Highlights component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './resources/styles.css';
 import Header from './components/header_footer/Header'
 import Featured from './components/featured'
 import VenueNfo from './components/venueNfo'
+import Highlights from './components/highlights'
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{height:"1500px", background:"lightblue"}}>
+      <div className="App">
         <Header />
         <Featured />
         <VenueNfo />
+        <Highlights />
       </div>
     );
   }
---
+++ src/components/highlights/Description.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Fade from 'react-reveal/Fade'
+
+const Description = () => {
+  return (
+    <Fade >
+    <div className="center_wrapper">
+        <h2>Highlights</h2>
+        <div className="highlight_description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis modi pariatur fugiat neque a provident sed, maiores aperiam blanditiis, earum perferendis architecto atque cumque repellat asperiores placeat sequi reiciendis dolor!
+        </div>
+    </div>
+    </Fade>
+  )
+}
+
+export default Description
